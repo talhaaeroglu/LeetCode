60786f41c4ea3ce4b80be5611141033378567c58
Talha Eroğlu <80883132+talhaaeroglu@users.noreply.github.com>
2022-10-04 00:05:36 +0300

    Time: 19 ms (24.60%), Space: 6.9 MB (73.59%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> map;
+        unordered_set<char> mapped;
+        if(s.size() != t.size()) return false;
+
+        for(int i=0; i< s.size(); i++){
+            int cs = s[i], ct = t[i];
+            if(map.count(cs)){
+                if(map[cs] == ct) continue;
+                else return false;
+            }
+
+            else{
+                if(mapped.count(ct)) return false;
+                map[cs] =ct;
+                mapped.insert(ct);
+            }
+
+        }
+    return true;
+    }
+};
