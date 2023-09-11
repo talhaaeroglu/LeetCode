1db61b31bb991f95b7a778ffce7202ff0b3498ab
Talha Eroğlu <80883132+talhaaeroglu@users.noreply.github.com>
2023-09-11 17:11:01 +0300

    Time: 139 ms (66.13%), Space: 17.6 MB (81.16%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -1,16 +1,8 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         if len(word1) != len(word2): return False
-        hm1 = {}
-        hm2 = {}
-        for i in range(len(word1)):
-            if word1[i] in hm1:
-                hm1[word1[i]] += 1
-            if word1[i] not in hm1:
-                hm1[word1[i]] = 1
-            if word2[i] in hm2:
-                hm2[word2[i]] += 1     
-            if word2[i] not in hm2:
-                hm2[word2[i]] = 1     
+        
+        counter1 = Counter(word1)
+        counter2 = Counter(word2) 
 
-        return set(hm1.keys()) == set(hm2.keys()) and sorted(hm1.values()) == sorted(hm2.values())
+        return set(counter1.keys()) == set(counter2.keys()) and sorted(counter1.values()) == sorted(counter2.values())
